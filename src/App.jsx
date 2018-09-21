cdf960adad78256f19abc96101f15f54b7c767e2
Amine Sadry <amine.sadry@gmail.com>
2018-09-21 14:51:57 +0200

    use jsx extension for jsx files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from './pages/HomePage';
+import './App.less';
+
+const App = () => (
+  <div className="App">
+    <HomePage />
+  </div>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,6 @@ ReactDOM.render(
   <IntlProvider locale="en">
     <App />
   </IntlProvider>,
-  document.getElementById('root')
+  document.getElementById('root') // eslint-disable-line no-undef
 );
 registerServiceWorker();
